24221ee5cb240162605983e55190b984219059a6
dliu9395 <90239277+dliu9395@users.noreply.github.com>
2023-01-02 15:21:44 +1100

    Update stock_price_app.py

--- stock_price_app/stock_price_app.py
+++ stock_price_app/stock_price_app.py
@@ -5,34 +5,41 @@ import streamlit as st
 
 #Getting the data 
 pltr_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=PLTR&apikey=JCVIFL6CJKOZX0UV'
-pltr_response = requests.get(pltr_url) 
-pltr_data = pltr_response.json() 
-
 IVV_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IVV&apikey=JCVIFL6CJKOZX0UV'
-IVV_response = requests.get(IVV_url) 
-IVV_data = IVV_response.json() 
+
+def get_data(url): 
+    response = requests.get(url) 
+    return response.json()
+
+pltr_data = get_data(pltr_url) 
+IVV_data = get_data(IVV_url) 
 
 #Getting the most recent date and closing price 
-pltr_time_series_data = pltr_data['Time Series (Daily)']
-date = list(pltr_time_series_data.keys())[0]
-pltr_closing_price = float(pltr_time_series_data[date]['4. close'])
+def time_series_data(data): 
+    return data['Time Series (Daily)']
 
-IVV_time_series_data = IVV_data['Time Series (Daily)']
-IVV_closing_price = float(IVV_time_series_data[date]['4. close'])
+pltr_time_series_data = time_series_data(pltr_data) 
+IVV_time_series_data = time_series_data(IVV_data)
+current_date = list(pltr_time_series_data.keys())[0]
+
+def current_closing_price(data): 
+    return float(data[current_date]['4. close']) 
+
+pltr_closing_price = current_closing_price(pltr_time_series_data)
+IVV_closing_price = current_closing_price(IVV_time_series_data)
 
 #Daily percentage Change in price 
-pltr_second_last_date = list(pltr_time_series_data.keys())[1]
-pltr_second_last_day_closing_price = float(pltr_time_series_data[pltr_second_last_date]['4. close'])
-pltr_percentage_change = (pltr_closing_price/pltr_second_last_day_closing_price) - 1 
-pltr_percentage_change = "{:.2%}".format(pltr_percentage_change)
+def percentage_change(data, closing_price): 
+    second_last_date = list(data.keys())[1] 
+    second_last_day_closing = float(data[second_last_date]['4. close'])
+    percentage_change = (closing_price/second_last_day_closing) - 1 
+    return "{:.2%}".format(percentage_change)
 
-IVV_second_last_date = list(IVV_time_series_data.keys())[1]
-IVV_second_last_day_closing_price = float(IVV_time_series_data[IVV_second_last_date]['4. close'])
-IVV_percentage_change = (IVV_closing_price/IVV_second_last_day_closing_price) - 1 
-IVV_percentage_change = "{:.2%}".format(IVV_percentage_change)
+pltr_percentage_change = percentage_change(pltr_time_series_data, pltr_closing_price)
+IVV_percentage_change = percentage_change(IVV_time_series_data, IVV_closing_price)
 
 st.title("Comparing PLTR Daily Closing Price") 
-st.header("Date: {}".format(date))
+st.header("Date: {}".format(current_date))
 col1, col2 = st.columns(2)
 col1.metric(label = 'PLTR Closing Price', value = "${}".format(pltr_closing_price), delta = "{} daily change".format(pltr_percentage_change))
 col2.metric(label = 'S&P500 Index ETF Closing Price', value = "${}".format(IVV_closing_price), delta = "{} daily change".format(IVV_percentage_change))
